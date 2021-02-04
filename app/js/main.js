d8550ab3c523081f2985af7d3e9ed8552d0e9f80
pablorgarcia <pablo.ramon.garcia.fernandez@gmail.com>
2021-02-04 17:58:10 +0100

    initial commit

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+
+/*
+Que idioma por defecto tiene el navegador
+mapeamos con languages.js
+construimos con un bucle los elementos del DOM y enviamos el idioma correcto parrafo a parrafo.
+
+- Por defecto inglés
+*/
+
+
+/*
+Creamos las opciones del select con los idiomas disponibles que tengamos
+llamamos a una funcion constructora de elementos con la opcion seleccionada
+*/
+
+
+document.addEventListener('DOMContentLoaded', () => {});
