f43df4d56ac0172b4fa90dd193825c1c63b9eb16
pablorgarcia <pablo.ramon.garcia.fernandez@gmail.com>
2021-02-14 03:06:30 +0100

    select option input for change the translation working

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
+/*
+  - Desde el principio, que esté seleccionado el lenguaje actual ene le select option input
+  - Olvidar el primer texto del array de traducciones
+*/
+
 let matchLang = [];
 let newTranslation = availableLangs[0];
 let readyToTranslation = false;
@@ -21,33 +26,56 @@ if (navigator.language != 'en') {
 // Creating a new P element
 const newPElement = (textToInnerHTML, elementFather) => {
   console.log('creating new P element');
-  const pText = document.createElement('p');
+  const pEl = document.createElement('p');
   const contentText = document.createTextNode(textToInnerHTML);
-  elementFather.appendChild(pText);
-  pText.appendChild(contentText);
+  elementFather.appendChild(pEl);
+  pEl.appendChild(contentText);
+};
+
+// Creating a new Option input element
+const newOptionElement = (textToInnerHTML, elementFather, valueToInnerHTML) => {
+  console.log('creating new OPTION input element');
+  const optionEl = document.createElement('option');
+  const contentText = document.createTextNode(textToInnerHTML);
+  elementFather.appendChild(optionEl);
+  optionEl.appendChild(contentText);
+  optionEl.value = valueToInnerHTML;
+};
+
+const onSetLang = () => {
+  const mainEl = document.getElementsByTagName('main')[0];
+  const childCount = mainEl.childElementCount;
+  // Removing the P elements from DOM
+  for (let i = 0; i < childCount; i++) { mainEl.children[0].remove() }
+  // Adding the new P elements on DOM with the correct translation
+  for (let i = 0; i < newTranslation.length; i++) { newPElement(newTranslation[i], mainEl) }
+  // Changing DOM HTML lang attribute for the user one
+  document.getElementsByTagName('html')[0].lang = matchLang;
+};
+
+// If we change the language from the select input
+const onChangeLang = (newLang) => {
+  if(newLang != undefined) {
+    newTranslation = availableLangs[newLang];
+    onSetLang();
+    console.log('lang changed to', newLang)
+  }
 };
 
 // When DOM is ready
 document.addEventListener('DOMContentLoaded', () => {
 
-  if (readyToTranslation) {
-    const mainEl = document.getElementsByTagName('main')[0];
-    const childCount = mainEl.childElementCount;
-    // Removing the P elements from DOM
-    for (let i = 0; i < childCount; i++) { mainEl.children[0].remove(); }
-    // Adding the new P elements on DOM with the correct translation
-    for (let i = 0; i < newTranslation.length; i++) { newPElement(newTranslation[i], mainEl); }
-    // Changing DOM HTML lang attribute for the user one
-    document.getElementsByTagName('html')[0].lang = matchLang;
-  } else {
-    console.log('Has not found lang for user');
-  }
+  if (readyToTranslation) { onSetLang() } 
+  else { console.log('Has not found lang for user') }
 
 /*
 Creamos las opciones del select con los idiomas disponibles que tengamos
 llamamos a una funcion constructora de elementos con la opcion seleccionada
 */
   const selectEl = document.getElementById('selectLanguage');
-  console.log('select input element',selectEl);
+
+  for (let i = 0; i < Object.keys(availableLangs).length; i++) {
+    newOptionElement(Object.values(availableLangs)[i][0], selectEl, Object.keys(availableLangs)[i]);
+  }
 
 });
